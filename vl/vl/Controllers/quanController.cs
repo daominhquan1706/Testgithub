58121cb326d5a2ea1f1530aeead478c79560e0c2
daominhquan1706 <daominhquan176@gmail.com>
2018-09-07 16:45:43 +0700

    creatquancontroller

---
+++ vl/vl/Controllers/quanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace vl.Controllers
+{
+    public class quanController : Controller
+    {
+        // GET: quan
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
