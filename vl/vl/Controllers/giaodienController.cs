ca2baa0f225784e99653be147f26066c13e06f49
GG <longnguyen1411997@gmail.com>
2018-09-07 16:45:33 +0700

    create giaodien CTL

---
+++ vl/vl/Controllers/giaodienController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace vl.Controllers
+{
+    public class giaodienController : Controller
+    {
+        // GET: giaodien
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
