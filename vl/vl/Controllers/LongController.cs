cdd7f7c04bface61310d48719ab26f10df4c5bbd
GG <longnguyen1411997@gmail.com>
2018-09-07 16:48:16 +0700

    Long CTL

---
+++ vl/vl/Controllers/LongController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace vl.Controllers
+{
+    public class LongController : Controller
+    {
+        // GET: Long
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
